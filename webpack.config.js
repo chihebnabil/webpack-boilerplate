2a166d03e2f3ba59dadb3c5003ae363219f440ab
CHIHEB Nabil <chiheb.design@gmail.com>
2017-11-28 21:37:18 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+module.exports = {
+    entry: ['./main.js', './main.scss'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{ // sass / scss loader for webpack
+            test: /\.(sass|scss)$/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'sass-loader'],
+                fallback: 'style-loader'
+            })
+        }]
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            test: /\.js($|\?)/i,
+            exclude: /\/node_modules/,
+            cache: false
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: { removeAll: true } },
+            canPrint: true
+          })
+    ]
+};
